56c30e0b270dd4f33e1de618d845cf86def57756
Saran Sasidharan <saran.s.dhar@gmail.com>
2020-12-18 23:55:18 +0530

    working game

--- src/app.js
+++ src/app.js
@@ -1,24 +1,48 @@
-const { App, LogLevel } = require("@slack/bolt");
+const RandomGuessGame = require("./game");
+const randomIntegerInInterval = require("./random");
 
-const logLevel =
-  process.env.PRODUCTION === "true" ? LogLevel.INFO : LogLevel.DEBUG;
+const db = {};
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  logLevel,
-});
-
-app.event("app_mention", async ({ event, say }) => {
-  await say(`Hey there <@${event.user}>!`);
-});
-
-const run = async () => {
-  const port = process.env.PORT || 3000;
-  await app.start(port);
-
-  console.debug(`Listening on port ${port}`);
-  console.log("⚡️ Bolt app is running!");
+const createNewGame = () => {
+  const noOfTries = 3;
+  const guessRange = { min: 1, max: 10 };
+  const randomNumberGenerator = () => randomIntegerInInterval(guessRange);
+  return new RandomGuessGame(noOfTries, randomNumberGenerator);
 };
 
-run();
+const startGame = (user) => {
+  const game = createNewGame();
+  db[user] = game;
+};
+
+const isPlayer = (user) => {
+  return db.hasOwnProperty(user);
+};
+
+const getUpdatedGameStatus = (user, guessValue) => {
+  const game = db[user];
+  const status = game.guess(guessValue);
+
+  if (status.guessedCorrect) {
+    delete db[user];
+    return "Congratulations! You guessed the number I was thinking of!";
+  }
+
+  if (status.gameOver) {
+    delete db[user];
+    return "Sorry! Your tries are over :(";
+  }
+
+  db[user] = game;
+
+  if (status.is_greater) {
+    return "The number I'm thinking of is lower";
+  }
+  return "The number I'm thinking of is higher";
+};
+
+module.exports = {
+  startGame,
+  isPlayer,
+  getUpdatedGameStatus,
+};
---
+++ src/main.js
@@ -0,0 +1,36 @@
+const { App, LogLevel } = require("@slack/bolt");
+const { startGame, isPlayer, getUpdatedGameStatus } = require("./app");
+
+const logLevel =
+  process.env.PRODUCTION === "true" ? LogLevel.INFO : LogLevel.DEBUG;
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  logLevel,
+});
+
+app.event("app_mention", async ({ event, say }) => {
+  startGame(event.user);
+  await say(
+    `Hey there <@${event.user}>!\nWelcome to the Slack Bot Game, try and guess what number I am thinking of between 0 and 10`
+  );
+});
+
+app.message(/^[1-9]/, async ({ message, context, say }) => {
+  if (!isPlayer(message.user)) {
+    return;
+  }
+  const guess = parseInt(context.matches[0]);
+  await say(getUpdatedGameStatus(message.user, guess));
+});
+
+const run = async () => {
+  const port = process.env.PORT || 3000;
+  await app.start(port);
+
+  console.debug(`Listening on port ${port}`);
+  console.log("⚡️ Bolt app is running!");
+};
+
+run();
